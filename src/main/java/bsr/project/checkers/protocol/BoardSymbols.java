e0bdb1889c2e4dde8f00c439ee8efda6de438404
iszulc <Ireneusz.Szulc@exorigo-upos.pl>
2017-04-26 16:02:35 +0200

    created protocol read/write interfaces

---
+++ src/main/java/bsr/project/checkers/protocol/BoardSymbols.java
@@ -0,0 +1,17 @@
+
+public class BoardSymbols {
+
+	/** biały pionek */
+	public static final char WHITE_PAWN = 'o';
+	/** biała damka */
+	public static final char WHITE_SUPER = 'w';
+
+	/** czarny pionek */
+	public static final char BLACK_PAWN = 'x';
+	/** czarna damka */
+	public static final char BLACK_SUPER = 'b';
+
+	/** puste pole */
+	public static final char EMPTY = ' ';
+	
+}
