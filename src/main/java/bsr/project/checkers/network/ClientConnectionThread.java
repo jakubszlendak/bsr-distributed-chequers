e0c0144d72c26d869bed1feafb136f0f52470cdc
iszulc <Ireneusz.Szulc@exorigo-upos.pl>
2017-04-26 14:22:43 +0200

    Added Client Connection Thread

---
+++ src/main/java/bsr/project/checkers/network/ClientConnectionThread.java
@@ -0,0 +1,77 @@
+class ClientConnectionThread extends Thread {
+
+   private Socket clientSocket;
+   private volatile boolean active = true;
+   BufferedReader in = null;
+   PrintWriter out = null;
+	
+   public ClientConnectionThread(Socket clientSocket) {
+      this.clientSocket = clientSocket;
+   }
+	
+   public void run() {
+
+      // TODO register for event closeAllConnections
+
+      // TODO new connection event
+
+      System.out.println("Accepted Client Address - " + clientSocket.getInetAddress().getHostName());
+      try {
+         in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+         out = new PrintWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+         
+         while(active) { 
+            String received = in.readLine();
+
+            if (received == null){
+
+               //end of the stream has been reached
+               // TODO send event disconnection;
+               
+               close();
+               break;
+
+            }else{
+
+               System.out.println("Client Says :" + received);
+               //TODO send event: received bytes from client, send to parser
+
+            }
+
+         }
+
+      } catch(Exception e) { 
+         Logs.error(e);
+      }
+
+      // TODO unregister from all events
+   }
+
+   public synchronized boolean isActive(){
+      return active;
+   }
+
+   public synchronized void close(){
+      if (active){
+         try { 
+            in.close(); 
+            out.close(); 
+            clientSocket.close(); 
+            active = false;
+            System.out.println("Connection closed");
+         } catch(IOException e) { 
+            Logs.error(e);
+         } 
+      }
+   }
+
+   public synchronized void sendLine(String line){
+      if (!active)
+         throw new InvalidStateException("Client connection is not active");
+      if (out == null)
+         throw new InvalidStateException("No output stream");
+      out.println(line);
+      out.flush();
+   }
+
+}
